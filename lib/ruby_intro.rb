8c26d6e4f1a1e04fe5faaeba2e5d7d93070349ef
monpeco <monpeco@gmail.com>
2016-06-17 01:55:03 +0000

    parte2 y 3 del hw1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -46,21 +46,68 @@ return false
 end
 
 # Part 2
-
+# Define a method hello(name) that takes a string representing a name and returns the string "Hello, " concatenated 
+# with the name. Run associated tests via: $ rspec -e '#hello' spec/part2_spec.rb
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
+
+# Define a method starts_with_consonant?(s) that takes a string and returns true if it starts with a consonant and 
+# false otherwise. (For our purposes, a consonant is any letter other than A, E, I, O, U.) NOTE: be sure it works for
+# both upper and lower case and for nonletters! Run associated tests via: $ rspec -e '#starts_with_consonant?' spec/part2_spec.rb
 def starts_with_consonant? s
-  # YOUR CODE HERE
+    s_lower=s.downcase
+    if /^[a-z&&[^aeiou]]/.match(s_lower)    
+      return true
+    else        
+      return false
+    end
 end
 
+
+
+# Define a method binary_multiple_of_4?(s) that takes a string and returns true if the string represents a binary number 
+# that is a multiple of 4. NOTE: be sure it returns false if the string is not a valid binary number! Run associated tests 
+# via: $ rspec -e '#binary_multiple_of_4?' spec/part2_spec.rb
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return true if s=="0"
+  num_decimal = s.to_i(2)
+  if num_decimal != 0 and num_decimal%4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
+# Define a class BookInStock which represents a book with an ISBN number, isbn, and price of the book as a floating-point 
+# number, price, as attributes. Run associated tests via: $ rspec -e 'getters and setters' spec/part3_spec.rb
 
+# The constructor should accept the ISBN number (a string, since in real life ISBN numbers can begin with zero and can 
+# include hyphens) as the first argument and price as second argument, and should raise ArgumentError (one of Ruby's 
+# built-in exception types) if the ISBN number is the empty string or if the price is less than or equal to zero. Include 
+# the proper getters and setters for these attributes. Run associated tests via: $ rspec -e 'constructor' spec/part3_spec.rb
+
+# Include a method price_as_string that returns the price of the book formatted with a leading dollar sign and two decimal places, 
+# that is, a price of 20 should format as "$20.00" and a price of 33.8 should format as "$33.80". Run associated tests 
+# via: $ rspec -e '#price_as_string' spec/part3_spec.rb
 class BookInStock
-# YOUR CODE HERE
+attr_accessor :isbn
+attr_accessor :price
+
+    def initialize(isbn, price)
+        raise ArgumentError if isbn.empty?      
+        raise ArgumentError if price.to_i<=0      
+        @isbn = isbn
+        @price = price
+    end
+    
+    def isbn=(new_isbn)
+      @isbn=new_isbn
+    end
+
+    def price_as_string
+      return "$" + sprintf("%.2f", @price)
+    end
 end
